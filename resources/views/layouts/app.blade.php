5ded2348ec052c4079f502f9b4666c21d3213d6f
AsZaychik <aszaychik@gmail.com>
2022-12-15 20:39:12 +0700

    Auth My Account

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -349,8 +349,8 @@
                                     </li>
                                     <li><a href="blog.html">Blog </a></li>
                                     <li><a href="contact.html">Contact</a></li>
+                                    @auth
                                     <li><a href="#">My Account<i class="fi-rs-angle-down"></i></a>
-                                        @auth
                                             @if (Auth::user()->utype == 'ADM')
 
                                                 <ul class="sub-menu">
