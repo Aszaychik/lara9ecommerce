33d8ae1673e32753a0361a3111604a9655901c0e
AsZaychik <aszaychik@gmail.com>
2022-12-16 15:46:54 +0700

    Products By Category

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\ProfileController;
 use App\Http\Livewire\Admin\AdminDashboardComponent;
 use App\Http\Livewire\CartComponent;
+use App\Http\Livewire\CategoryComponent;
 use App\Http\Livewire\CheckoutComponent;
 use App\Http\Livewire\DetailsComponent;
 use App\Http\Livewire\HomeComponent;
@@ -34,6 +35,8 @@ Route::get('/product/{slug}', DetailsComponent::class)->name('product.details');
 Route::get('/cart', CartComponent::class)->name('shop.cart');
 
 Route::get('/checkout', CheckoutComponent::class)->name('shop.checkout');
+
+Route::get('/product-category/{slug}', CategoryComponent::class)->name('product.category');
 
 // Route::get('/dashboard', function () {
 //     return view('dashboard');
