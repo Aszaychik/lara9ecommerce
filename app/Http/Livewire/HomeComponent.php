31d4bb4bc78dce2fc5857a7557f0274770edb83f
AsZaychik <aszaychik@gmail.com>
2022-12-19 06:13:03 +0700

    Admin Show Featured Products On Homepage

--- app/Http/Livewire/HomeComponent.php
+++ app/Http/Livewire/HomeComponent.php
@@ -5,13 +5,24 @@ namespace App\Http\Livewire;
 use App\Models\HomeSlider;
 use App\Models\Product;
 use Livewire\Component;
+use Cart;
 
 class HomeComponent extends Component
 {
+    public function store($product_id, $product_name, $product_price)
+    {
+        Cart::instance('cart')->add($product_id, $product_name, 1, $product_price)->associate('\App\Models\Product');
+
+        session()->flash('success_message', 'Item added in Cart');
+
+        return redirect()->route('shop.cart');
+    }
+
     public function render()
     {
         $slides = HomeSlider::where('status', 1)->get();
         $lproducts = Product::orderBy('created_at', 'DESC')->get()->take(8);
-        return view('livewire.home-component', ['slides' => $slides, 'lproducts' => $lproducts]);
+        $fproducts = Product::where('featured', 1)->inRandomOrder()->get()->take(8);
+        return view('livewire.home-component', ['slides' => $slides, 'lproducts' => $lproducts, 'fproducts' => $fproducts]);
     }
 }
